e74246850c4c6c62779f35ff7c0f19402c26f03d
iamkevingreen <thedudem4n@gmail.com>
2016-09-13 00:36:08 -0400

    add options for add/remove/toggle of classes

--- src/bradpitt.js
+++ src/bradpitt.js
@@ -4,12 +4,29 @@
 /**
  *  @param  {object|...array} el dom element/s
  *  @param  {string} className long to wait for a response
- *  @param  {number} iming in milliseconds
+ *  @param  {number} timing in milliseconds
+ *  @param  {options} options object
  */
 
-function lazy(el, className, timing) {
+function lazy(el, className, timing, options = {}) {
+  let change = options.change || 'add'
+  let classSwap = (e) => {
+    switch(change) {
+      case 'add':
+        e.classList.add(className)
+        break
+      case 'remove':
+        e.classList.remove(className)
+        break
+      case 'toggle':
+        e.classList.toggle(className)
+        break
+      default:
+        console.log('invalid value for change option specified')
+    }
+  }
   let sT = (e) => {
-    setTimeout(() => { e.classList.add(className) }, timing)
+    setTimeout(() => { classSwap(e) }, timing)
   }
   el.length ? Array.from(el, (e) => { sT(e) }) : sT(el)
   return this
